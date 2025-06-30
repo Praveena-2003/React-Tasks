72f1201e2fff3b8647d0f42058d9d682064336f2
Praveena-2003 <k.praveena1803@gmail.com>
2025-06-30 16:12:11 +0530

    Creating counter function using redux

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,46 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from '../features/counter/CounterSlice';
 
-function CounterApp() {
-    const [count, setCount] = useState(0);
 
-    const increment = () => {
-        setCount(count + 10);
-    };
 
-    const decrement = () => {
-        setCount(count - 1);
-    };
+const Counter = () => {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
 
-    const reset = () => {
-        setCount(0);
-    };
-
-    return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1>Simple Counter</h1>
-            <h2>{count}</h2>
-            <div style={{ marginTop: '20px' }}>
-                <button
-                    onClick={decrement}
-                    style={{ margin: '0 10px', padding: '8px 16px' }}
-                >
-                    Decrement
-                </button>
-                <button
-                    onClick={reset}
-                    style={{ margin: '0 10px', padding: '8px 16px' }}
-                >
-                    Reset
-                </button>
-                <button
-                    onClick={increment}
-                    style={{ margin: '0 10px', padding: '8px 16px' }}
-                >
-                    Increment
-                </button>
-            </div>
-        </div>
-    );
+    return(
+        <>
+         <h1>Redux Counter: {count}</h1>
+         <button onClick={() => dispatch(increment())}>+</button>
+         <button onClick={() => dispatch(decrement())}>-</button>
+         <button onClick={() => dispatch(reset())}>Reset</button>
+        
+        </>
+    )
 }
-
-export default CounterApp;
+export default Counter;
